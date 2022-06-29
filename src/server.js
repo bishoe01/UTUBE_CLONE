922f4da77ca7c22de4beaadb23cac59b0eeff5b8
bishoe01 <bishoe01@gmail.com>
2022-06-30 00:54:45 +0900

    add morgan from npm to precise respond message

--- src/server.js
+++ src/server.js
@@ -1,24 +1,25 @@
-import express from "express";
+import express, { response } from "express";
+import morgan from "morgan";
 //const express = require("express");
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`someone is going to : ${req.url}`);
-    next();     
-};
-
-const handleHome = (req, res, next) => {
+const handleHome = (req, res) => {
     return res.send("<h1>I STILL LOVE YOU<h1>");
 }
 const handleLogin = (req, res) => {
-    return res.send("login Here");
+    return res.send("<h1>Login</h1>");
 }
 
-app.get("/" , gossipMiddleware,handleHome);
-app.get("/login", gossipMiddleware,handleLogin);
-
+const handleProtected = (req, res) => {
+    return res.send("<h1>Welcome to the private Lounge</h1>");
+}
+app.use(logger);
+app.get("/",handleHome);
+app.get("/login",handleLogin);
+app.get("/protected",handleProtected);
 
 
 const handleListening = () => console.log(`server Listening on port ${PORT}🚀`);
