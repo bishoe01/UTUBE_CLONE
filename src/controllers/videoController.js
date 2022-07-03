aed9c459b61a152d999a61d85efb2791d64b9253
bishoe01 <bishoe01@gmail.com>
2022-07-04 01:46:39 +0900

    how to use data(test) with .pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,39 @@
 
-export const trending = (req, res) => res.render("home");
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.send("Edit video")};
+let videos = [
+    {
+        title : "First Video",
+        rating : 5,
+        comments : 2,
+        createdAt : "2 minutes ago",
+        views:59,
+        id:1,
+    },
+    {
+        title : "Second Video",
+        rating : 1,
+        comments : 2,
+        createdAt : "2 minutes ago",
+        views:1,
+        id:2,
+    },
+    {
+        title : "Third Video",
+        rating : 2,
+        comments : 3,
+        createdAt : "5 minutes ago",
+        views:202,
+        id:3,
+    }
+];
+
+export const trending = (req, res) => {
+    res.render("home",{pageTitle: "Home",videos});}
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    console.log(video);
+    res.render("watch",{pageTitle : `Watch ${video.title}`, video})};
+export const edit = (req, res) => res.render("edit",{pageTitle : "Edit"});
 export const search = (req, res) => {res.send("Search videos")};
 export const uploadVideo = (req, res) => {res.send("Upload video")};
 export const deleteVideo = (req, res) => {res.send("Delete video")};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { edit , see ,deleteVideo,uploadVideo } from '../controllers/videoController';
+import { edit , watch ,deleteVideo,uploadVideo } from '../controllers/videoController';
 const videoRouter = express.Router();
 
 
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 videoRouter.get("/upload", uploadVideo);
+
+
+
 export default videoRouter;
