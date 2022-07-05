7758504c747b93c6ab7b9a38b7a663b64e39a63c
bishoe01 <bishoe01@gmail.com>
2022-07-05 16:57:12 +0900

    add upload url to add new video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -36,4 +36,27 @@ export const getEdit = (req, res) => {
     const { id } = req.params;
     const video = videos[id - 1];
     return res.render("edit",{pageTitle : `Editing : ${video.title}`, video})};
-export const postEdit = (req,res) => {};
+export const postEdit = (req,res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    videos[id - 1].title = title;
+    return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+    return res.render("upload",{pageTitle : `Upload Video`});    
+};
+export const postUpload = (req, res) => {
+    const {title} = req.body;
+    const newVideo = {
+        title,
+        rating : 0,
+        comments : 0,
+        createdAt : "just now",
+        views: 222,
+        id:videos.length + 1,
+    }
+    videos.push(newVideo);
+    return res.redirect("/");
+};
+
